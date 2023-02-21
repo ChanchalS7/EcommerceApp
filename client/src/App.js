32f502937d68bd6c2cfaeb4a4178dfcdccd82156
ritu1011 <chsverma7@gmail.com>
2023-02-22 01:23:29 +0530

    add frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
-
+import { Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import HomePage from './pages/HomePage';
+import Pagenotfound from "./pages/Pagenotfound";
+import Policy from "./pages/Policy";
 function App() {
   return (
-    <div className="App">
-      <h1>Hello, Chanchal!</h1>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/policy" element={<Policy />} />
+        <Route path="*" element={<Pagenotfound />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ client/src/components/Layout/Layout.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Footer from './Footer'
+import Header from './Header'
+const Layout = (props) => {
+	return (
+		<div>
+			<Header />
+			<main style={{
+				minHeight: '80vh'
+			}}>{props.children}</main>
+			<Footer />
+		</div>
+	)
+}
+
+export default Layout
+
+// you can destructure the props like : {children} in argument 
