1e4e63b310f4e25feb7acac9a2e4bfce11aa5f6a
ritu1011 <chsverma7@gmail.com>
2023-02-23 16:57:35 +0530

    add packages

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import colors from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
+import cors from "cors"
 import connectDB from './config/db.js';
 import authRoutes from './routes/authRoute.js'
 //Configure env
@@ -12,6 +13,7 @@ connectDB();
 const app = express();
 
 //Middlewares
+app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
 
